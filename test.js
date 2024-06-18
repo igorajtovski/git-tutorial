3d202ae15e7545474fddd10e4b1b654c43a074f1
igorajtovski <igorajtovski@gmail.com>
2024-06-18 16:07:29 +0200

    NavBarV2

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
 console.log("test git commands");
+//dodaden nov branch i posledniot commit NavBarVersion1 e napraven na novit branch
+//branchot e dodaden samo lokalno... da se dodade i na remote
+// da se napravi merge na branchovite....
